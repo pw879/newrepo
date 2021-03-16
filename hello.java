7b89c348a6e7c86194eeb179bd562a3f40279f9c
Paul Wilson <pwilson@rialtostudio.com>
2021-03-15 23:46:49 -0500

    bug fixed

--- hello.java
+++ hello.java
@@ -2,6 +2,7 @@ public class hello{
 	public static void main(String[] args){
 		System.out.println("Hello World");
 		System.out.println("Adding one more line");
-		System.out.println("making some changes"); 
+		System.out.println("making some changes");
+		System.out.println("Done with the changes"); 
 }
 }
